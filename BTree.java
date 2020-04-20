f4ae125dfebc0d475b2d144280cbe38280357ccc
rachelsingleton <rachelsingleton@u.boisestate.edu>
2020-04-20 15:41:11 -0700

    Created the BTreeNode file; started on createBTree method and the constructor - need to know structure of methods in BTreeNode for things to work correctly and it is possible that no createBTree method is needed since we have the constructor

--- BTree.java
+++ BTree.java
@@ -1,11 +1,33 @@
 /* This is a simple Java program.
    FileName : "HelloWorld.java". */
-class BTree
+public class BTree
 {
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
+    private BTreeNode[] btree;
+    private BTreeNode root;
+
+    /* Constructor
+    Creates a BTree with only one node
+    Set properties of that node to make it an empty root node
+     */
+    public BTree() {
+        btree = new BTreeNode[1];
+        root = btree.getNode(); //TODO need methods similar to this from BTreeNode
+        root.setRoot() = True; //TODO need methods similar to this from BTreeNode
+        root.setLeaf() = True; //TODO need methods similar to this from BTreeNode
+        root.numKeys() = 0; //TODO need methods similar to this from BTreeNode
+
+        //TODO need to write the root to disk now
+
     }
+
+    /*
+    Creates the first (empty) root node
+    Only needs to be called once for each BTree since the other methods will add nodes
+    @param supposed to take in T which is the BTree we are building
+     */
+    public BTree createBTree() {
+        return null;
+    }
+    //TODO is above method actually needed?
 }
+
