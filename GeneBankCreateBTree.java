fe770f8b77055986164b25991bc76ddb1faa218f
Zach Sherwood <zachsherwood@u.boisestate.edu>
2020-04-21 09:23:11 -0600

    Test

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -1,13 +1,38 @@
 
 public class GeneBankCreateBTree {
 
-	public GeneBankCreateBTree() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		System.out.println("test");
+		
+//usage is:java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]
+		try {
+			if (args.length < 4 || args.length  > 6) {
+				usage();
+		}
+			//Todo: logic dealing with cache
+			if (!args[0].equals(1) || !args[0].equals(0)) {
+				usage();
+			}
+			
+			//Todo: logic dealing with degree
+			int degree = Integer.parseInt(args[1]);
+			if ( degree < 0 || degree > 31 ) {
+				usage();
+			}
+			
+			if (degree == 0) {
+				System.out.println("the ideal degree formula");
+			}
+			
+			
+			
+			
+
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void usage() {
+		System.out.println(
+				"Usage is: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length>[<cache size>] [<debug level>]");
 	}
 
 }
