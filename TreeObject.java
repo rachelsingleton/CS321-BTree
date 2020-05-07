9eefc416f796398a0449c7fc60c211cb5e5cec09
Zach Sherwood <zachsherwood@u.boisestate.edu>
2020-05-06 18:37:23 -0600

    added toString to TreeObject (still need to test)

--- TreeObject.java
+++ TreeObject.java
@@ -1,47 +1,66 @@
 public class TreeObject {
 
-    //Instance Variables
-    private long key;
-    private int frequency;
+	// Instance Variables
+	private long key;
+	private int frequency;
+	private long substring;
 
+	// Constructor
+	/*
+	 * Takes in the key as a substring of type long (should be binary by this point)
+	 * Frequency always starts at 1
+	 */
+	public TreeObject(long substring) {
+		key = substring;
+		frequency = 1;
+	}
 
-    // Constructor
-    /*
-    Takes in the key as a substring of type long (should be binary by this point)
-    Frequency always starts at 1
-     */
-    public TreeObject(long substring)  {
-        key = substring;
-        frequency = 1;
-    }
+	/*
+	 * Increments the frequency by one
+	 */
+	public void incrementFreq() {
+		frequency++;
+	}
 
-    /*
-    Increments the frequency by one
-     */
-    public void incrementFreq() {
-        frequency++;
-    }
+	/*
+	 * Returns the frequency
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
 
-    /*
-    Returns the frequency
-     */
-    public int getFrequency() {
-        return frequency;
-    }
+	/*
+	 * Returns the key (aka gene substring)
+	 */
+	public long getString() {
+		return key;
+	}
 
-    /*
-    Returns the key (aka gene substring)
-     */
-    public long getString() {
-        return key;
-    }
+	/*
+	 * Printing string
+	 * 
+	 */
+	public String toString() {
 
-    /*
-    TODO: Zach said he would do this!!!!
-    Printing string and frequency for dump file
-     */
-    public String toString() {
+		String data = Long.toString(substring, 2);
 
-        return null;
-    }
+		if (data.length() % 2 != 0 && data.charAt(0) == '0') {
+			data = data.replaceFirst("0", "A");
+		} else if (data.length() % 2 != 0 && data.charAt(0) == '1') {
+			data = data.replaceFirst("1", "C");
+		}
+
+		for (int i = 0; i < data.length() - 1; i++) {
+			if (data.charAt(i) == '1' && data.charAt(i + 1) == '1') {
+				data = data.replaceFirst("11", "T");
+			} else if (data.charAt(i) == '1' && data.charAt(i + 1) == '0') {
+				data = data.replaceFirst("10", "G");
+			} else if (data.charAt(i) == '0' && data.charAt(i + 1) == '1') {
+				data = data.replaceFirst("01", "C");
+			} else if (data.charAt(i) == '0' && data.charAt(i + 1) == '0') {
+				data = data.replaceFirst("00", "A");
+			}
+		}
+		return data;
+	}
 }
